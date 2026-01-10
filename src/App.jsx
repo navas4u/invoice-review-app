35086f0c97e24fe1dca15fb0731ef4f1ed8c74e7
Navas Abdulkader <navas4u@gmail.com>
2026-01-10 13:17:04 +0300

    Heading App Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import PendingInvoices from "./pages/PendingInvoices";
 import ApprovedInvoices from "./pages/ApprovedInvoices";
 
 function App() {
-  <header className="bg-white border-b shadow-sm px-6 py-4 flex items-center justify-between">
-  <h1 className="text-2xl font-semibold text-gray-800">
-    Ezinvy <span className="text-sm text-gray-500 ml-2">Invoice Review</span>
-  </h1>
-  </header>
-  return (
-    <div className="App">
+ return (
+      <div className="App">
+       <header className="bg-white border-b shadow-sm px-6 py-4 flex items-center justify-between">
+        <h1 className="text-2xl font-semibold text-gray-800">
+          Ezinvy <span className="text-sm text-gray-500 ml-2">Invoice Review</span>
+        </h1>
+      </header>
       <nav className="p-4 bg-gray-100">
         <Link className="mr-4" to="/">
           Pending
